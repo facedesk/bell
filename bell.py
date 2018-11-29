7a03185de92572ef80d9baad8052bb8239961d92
David Gold <dgold@SJNMA.ORG>
2018-11-29 14:49:04 -0600

    got scheduler in, and ringing internal speaker

--- bell.py
+++ bell.py
@@ -11,19 +11,13 @@ import logging
 import urllib.request as urllib2
 import json
 import datetime
+import time
+import winsound
 
 
-def get_times_to_ring():
-    # JSON Representation
-    url = 'https://spreadsheets.google.com/feeds/list/0Akgh73WhU1qHdFg4UmRhaThfUFNBaFR3N3BMVW9uZmc/od6/public/basic?prettyprint=true&alt=json'
-    #      https://spreadsheets.google.com/feeds/list/<spreadsheet_key>/<worksheet_id>/public/basic
-    #      
+
+def get_times_to_ring():     
     url = 'https://spreadsheets.google.com/feeds/list/1Baj0j80ewjzpnL8GOfGpdqbnxAsueIl-DHCLTQ7PO70/od6/public/basic?prettyprint=true&alt=json'
-
-
-    #https://spreadsheets.google.com/feeds/worksheets/1Baj0j80ewjzpnL8GOfGpdqbnxAsueIl-DHCLTQ7PO70/private/full
-
-    #https://docs.google.com/spreadsheets/d//edit?usp=sharing
     response = urllib2.urlopen(url)
     html = response.read()
     html = json.loads(html)
@@ -33,13 +27,41 @@ def get_times_to_ring():
     #monday is 0, saturday is 6
     row = html['feed']['entry'][day]['content']['$t'].encode('utf-8').strip()   
 
-    times = list(map(lambda x:x.split(":")[1],str(row).split(",")))[1:]
+    times = list(map(lambda x:x.split(":")[1].strip().replace("'",""),str(row).split(",")))[1:]
     return times
 
+def ring_the_bell():
+    print("DING!!!!")
+    notes = [1521,1809,2280,3043,2280,3043]
+    for note in notes[:-1]:
+        winsound.Beep(note,250)
+    
+    winsound.Beep(notes[-1], 1000)
 
 
 if __name__ == "__main__":
-    print(get_times_to_ring())
-    #print ('column1:%s column2:%s column3:%s' % (column1, column2, column3)    )
-    
+    day_sheet_checked = 0
+    times = get_times_to_ring()
+    rung = [False for i in range(len(times))]
+    while 1:    
+        now = datetime.datetime.now()
+        timestr = "%H%M"
+        sj_time = now.strftime(timestr)
+        index = -1
+        if(now.hour == 8 and day_sheet_checked != now.day):
+            times = get_times_to_ring()
+            rung = [False for i in range(len(times))]
+            day_sheet_checked = now.day        
+        try:
+            index = times.index(sj_time)
+            if(not rung[index]):
+                ring_the_bell()
+                rung[index] = True
+        except:
+            index = -1
+
+
+        time.sleep(5)#it doesn't make a difference if the bell is 5 seconds late
+
+
 
